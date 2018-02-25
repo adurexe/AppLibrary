21d67c927bcb0115ebc69c2694551b8d93a0e3d7
Chunfu <adurexe@gmail.com>
2018-02-26 00:42:26 +0800

    init project

---
+++ app/src/main/java/com/adurexe/library/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.adurexe.library
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
